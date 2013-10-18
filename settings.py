0fc22045ca045075cba5f1fad6b10a8876ea0c17
jingchao <jingchaohu@gmail.com>
2013-10-18 14:55:33 +0800

    resovled conflict

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ envs = {
             'ec2-72-44-53-84.compute-1.amazonaws.com:9160',
             'ec2-184-73-45-244.compute-1.amazonaws.com:9160',
         ],
-        'MONGO_HOST': 'ec2-54-227-0-188.compute-1.amazonaws.com:27017', 
+        'MONGO_HOST': 'ec2-54-227-0-188.compute-1.amazonaws.com:27017',
     },
 }
 
