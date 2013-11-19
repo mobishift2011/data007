2cca7211ade10cfe70031b85ff43bd59da4111fa
root <root@ubuntu.(none)>
2013-11-19 11:09:46 +0800

    add

--- debouncing.py
+++ debouncing.py
@@ -3,10 +3,19 @@
 
 import time
 
+
+def replace_old(ret_bin):
+    try:
+        if len(bin(int(ret_bin))) != 58:
+            ret_bin = None
+    except:
+        ret_bin = None
+    return ret_bin
+        
 def get_update_bin(ret_bin, info):
     '''
     latest_buy_time
-                                                                共用59位
+             56
         31(ts), 21(num_sold30), 4(offset)
     '''
     if info.has_key('latest_buy_time'):
@@ -14,6 +23,8 @@ def get_update_bin(ret_bin, info):
     else:
         latest_time = 0
         
+    ret_bin = replace_old(ret_bin)
+    
     if ret_bin:
         cts, uts, offset = unpack_bin(int(ret_bin))
         print cts, uts, offset
@@ -39,6 +50,7 @@ def can_update(store_bin):
     if store_bin is None:
         return True
     else:
+        store_bin = replace_old(store_bin)
         cts, uts, offset = unpack_bin(store_bin)
         if offset == 0:
             offset = 80000
