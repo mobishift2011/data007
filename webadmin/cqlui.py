cd6ca834e6170722e82c0b08e6ca5724f901d653
ethan <mobishift@gmail.com>
2013-11-19 17:42:09 +0800

    add column select when query

--- webadmin/cqlui.py
+++ webadmin/cqlui.py
@@ -49,6 +49,7 @@ def cqlui_table(table):
     page = int(request.args.get('page', 1))
     limit = int(request.args.get('limit', 20))
     order_by = request.args.get('order_by')
+    column_clause = ''
     limit_clause = 'limit {}'.format(limit)
     where_clause = ''
     order_by_clause = ''
@@ -58,28 +59,45 @@ def cqlui_table(table):
             for k, v in request.form.iteritems():
                 if not k or not v:
                     continue
+
+                if k == 'select_columns':
+                    for column in request.form.getlist(k):
+                        column_clause += ', {}'.format(column) \
+                            if column_clause else '{}'.format(column)
+                    continue
+
                 if not where_clause:
                     where_clause = 'where {} = {}'.format(k, v)
                 else:
                     where_clause += 'and {} = {}'.format(k, v)
 
+        if not column_clause:
+            column_clause = '*'
         if order_by:
             order_by_clause = 'order by {} {}'.format(order_by, request.args.get('scend')).strip()
 
         count = 1000000000000
-        # count = int(db.execute('select count(*) from {}.{} {}'.format(\
-        #     KEYSPACE, table, where_clause), result=True)[1][0][0])
-        columns, rows = db.execute('select * from {}.{} {} {} {}'.format(\
-            KEYSPACE, table, where_clause, order_by_clause, limit_clause), result=True)
+        cql = 'select {} from {}.{} {} {} {}'.format(\
+            column_clause, KEYSPACE, table, where_clause, order_by_clause, limit_clause)
+        columns, rows = db.execute(cql, result=True)
     except:
         traceback.print_exc()
         count = 0
         try:
-            columns, rows = db.execute('select * from {}.{} {}'.format(\
-                KEYSPACE, table, limit_clause), result=True)
+            columns, rows = db.execute('select {} from {}.{} {}'.format(\
+                column_clause, KEYSPACE, table, limit_clause), result=True)
             rows = []
         except:
             traceback.print_exc()
 
-    return render_template('cqlui/table.html', \
-        columns=columns, rows=rows, table=table, **paginate(count=count, page=page, limit=limit))
+    full_columns = db.execute("select column_name from system.schema_columns \
+        where keyspace_name='{}' and columnfamily_name='{}';".format(KEYSPACE, table), result=True)[1]
+
+    return render_template(
+        'cqlui/table.html',
+        columns = columns,
+        rows = rows,
+        table = table,
+        full_columns = full_columns,
+        **paginate(count=count, page=page, limit=limit)
+    )
